5f5d55166b5a7e78405f34b55820f9584b8597ea
Golam Murtoba Ali <88472436+GolamMurtobaAli@users.noreply.github.com>
2023-11-28 19:51:13 +0530

    Delete practice directory

--- practice/src/main/java/com/example/practice/Repository/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.practice.Entity.Orders;
-
-public interface OrdersRepository extends JpaRepository<Orders,Long>{
-    
-}
--- practice/src/main/java/com/example/practice/Repository/PrescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.practice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.practice.Entity.DiagnosticTest;
-
-public interface PrescriptionRepository extends JpaRepository<DiagnosticTest, Long>{
-    
-}
